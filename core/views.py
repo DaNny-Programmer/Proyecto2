3eae37c3edb29338ee121d50974bf4ea7694a9c8
Daniela Muñoz <daniimhdez08@gmail.com>
2020-05-24 20:00:39 -0500

    Primer Modificacion de la segunda parte

--- core/views.py
+++ core/views.py
@@ -17,8 +17,7 @@ def store(request):
 def contact(request):
     return render(request, "core/contact.html")
 
-def blog(request):
-    return render(request, "core/blog.html")
+
 
 def sample(request):
     return render(request, "core/sample.html")
